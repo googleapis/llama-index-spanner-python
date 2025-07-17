3113a559623b978401a7ad32a08cbae8518ae37d
Shreya Goel <goelshreya@google.com>
2025-07-17 12:08:42 +0000

    test:fix duplicate graph issues

--- tests/test_graph_retriever.py
+++ tests/test_graph_retriever.py
@@ -29,7 +29,7 @@ from tests.utils import get_random_suffix, get_resources
 def setup(schema_type):
     """Setup the index for integration tests."""
     graph_store, _, llm, embed_model = get_resources(
-        schema_type,
+        schema_type + "_" + get_random_suffix(),
         clean_up=True,
         use_flexible_schema=schema_type == "flexible",
     )
@@ -91,7 +91,7 @@ def test_graph_retriever():
 
 def setup2(schema_type):
     graph_store, _, llm, embed_model = get_resources(
-        schema_type + "_2",
+        schema_type + "_" + get_random_suffix(),
         clean_up=True,
         use_flexible_schema=schema_type == "flexible",
     )
